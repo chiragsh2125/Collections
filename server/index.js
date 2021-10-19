ed70f999364ce765b57cb58f98538da39929fd96
Chirag Sharma <chiragsh2125@gmail.com>
2021-10-19 23:07:03 +0530

    build commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+mongoose.connect('mongodb+srv://chirag:chirag21@cluster0.c7wcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+app.listen(5000, () => {
+  console.log('Serving on port no.5000')
+})
